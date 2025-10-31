fd41581d4f2e1eaf21dead1dca1ec5b2259269c6
Dupong00 <tjdgns9954@gmail.com>
2025-10-31 18:04:53 +0900

    fix: 입력 공백 처리 기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,6 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String read(){
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 }
