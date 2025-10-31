21c86fc12353a9536074302b8a1b8448144babd4
Dupong00 <tjdgns9954@gmail.com>
2025-10-31 17:30:54 +0900

    feat: 결과 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,16 +1,17 @@
 package lotto;
 
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
 public class OutputView {
+    public void print(){
+        System.out.println();
+    }
     public void printMoneyQuestion() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-
     public void printCount(int count) {
-        System.out.println();
         System.out.println(count + "개를 구매했습니다.");
     }
     public void printWinningLottoQuestion() {
@@ -26,5 +27,30 @@ public class OutputView {
     }
 
     public void printTotalWinningStatus(Map<RANK, Integer> winningCount) {
+        DecimalFormat df = new DecimalFormat("###,###");
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for(Map.Entry<RANK, Integer> entry : winningCount.entrySet()) {
+            RANK key = entry.getKey();
+
+            if (key == RANK.MISS){
+                continue;
+            }
+
+            int matchCount = key.getMatchCount();
+            int value = entry.getValue();
+            int money = key.getPrizeMoney();
+
+            if(key == RANK.SECOND){
+                System.out.println(matchCount + "개 일치, 보너스 볼 일치 (" + df.format(money) + "원) - " + value + "개");
+                continue;
+            }
+
+            System.out.println(matchCount + "개 일치 (" + df.format(money) + "원) - " + value + "개");
+        }
+    }
+    public void printROI(double roi){
+        System.out.println("총 수익률은 " + roi + "%입니다.");
     }
 }
